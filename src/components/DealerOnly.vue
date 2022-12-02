6152ec856a04585dc7e5cb6f4a577e9d44c9dd9c
Thomas Thorin <thomas.thorin@gmail.com>
2022-12-02 14:23:53 +0100

    refinement dealeronly

--- src/components/DealerOnly.vue
+++ src/components/DealerOnly.vue
@@ -3,15 +3,18 @@ import { ref, computed, watchEffect, watch } from 'vue';
 import { getShuffledCards } from '../js/cards'
 import PlayingCard from './PlayingCard.vue';
 import Timer from './Timer.vue';
+import DealerOnlyInstructions from './DealerOnlyInstructions.vue';
 
 const emit = defineEmits(['updateDisplay'])
-const shoe = getShuffledCards(1)
+let shoe = getShuffledCards(1)
 const hand = ref([shoe.pop(), shoe.pop()])
 const cardTotalTwo = computed(() => getCardTotalTwo(hand.value))
 const answers = ref(getAnswers())
 const wrongAnswers = ref(0)
 const timerShouldRun = ref(false)
 const gameOver = ref(false)
+const resetTimer = ref(false)
+const showInstructions = ref(false)
 
 const goToMain = () => {
 	emit('updateDisplay', "main")
@@ -33,7 +36,7 @@ function getCardTotalTwo(cards) {
 		answer -= 10
 		numberOfAces--
 	}
-	return answer
+	return { answer, numberOfAces }
 }
 
 const dealCard = () => {
@@ -44,7 +47,14 @@ function getAnswers() {
 	const randomNumberZeroToFour = Math.floor(Math.random() * 4)
 	let newAnswers = []
 	for (let i = 0; i < 4; i++) {
-		newAnswers.push(cardTotalTwo.value - randomNumberZeroToFour + i)
+		let newAnswer = { value: cardTotalTwo.value.answer - randomNumberZeroToFour + i }
+		if (cardTotalTwo.value.numberOfAces > 0 && newAnswer.value < 17 && newAnswer.value > 11) {
+			newAnswer.display = `${newAnswer.value - 10} / ${newAnswer.value}`
+		}
+		else {
+			newAnswer.display = newAnswer.value
+		}
+		newAnswers.push(newAnswer)
 	}
 	return newAnswers
 }
@@ -55,7 +65,7 @@ watch(() => hand.value[hand.value.length - 1].id, () => {
 })
 
 const checkShouldDealOrReset = () => {
-	if (cardTotalTwo.value >= 17) {
+	if (cardTotalTwo.value.answer >= 17) {
 		if (shoe.length < 10) {
 			timerShouldRun.value = false
 			gameOver.value = true
@@ -72,7 +82,7 @@ const checkAnswer = (answer) => {
 		timerShouldRun.value = true
 	}
 
-	if (answer === cardTotalTwo.value) {
+	if (answer === cardTotalTwo.value.answer) {
 		checkShouldDealOrReset()
 	} else {
 		wrongAnswers.value++
@@ -91,47 +101,74 @@ const updateBestTimes = (time) => {
 		newBestTimes = []
 	}
 	newBestTimes.push(entry)
-	newBestTimes.sort((a, b) => a.time - b.time)
+	newBestTimes.sort((a, b) => a.mistakes - b.mistakes || a.time - b.time)
 	localStorage.setItem("bestTimes", JSON.stringify(newBestTimes.slice(0, 5)))
 }
 const reset = () => {
-	shoe.value = getShuffledCards(1)
+	shoe.clear
+	shoe = getShuffledCards(1)
 	hand.value = [shoe.pop(), shoe.pop()]
 	wrongAnswers.value = 0
 	gameOver.value = false
 	timerShouldRun.value = false
+	resetTimer.value = true
 }
 </script>
 
 <template>
 
-	<Timer :shouldRun="timerShouldRun" @timeAtStop="(time) => updateBestTimes(time)" />
-	<div v-if="!gameOver" class="dealer-only-container">
-		<div class="dealer-only-card-hand">
-			<PlayingCard v-for="card in hand" :card="card" :key="card.id" />
+	<span @click="showInstructions = !showInstructions" class="instructions-button">{{ showInstructions ? "←" : "?"
+	}}</span>
+	<font-awesome-icon icon="fa-regular fa-circle-question" />
+	<div v-if="!showInstructions">
+		<Timer :shouldRun="timerShouldRun" :shouldReset="resetTimer" @timeAtStop="(time) => updateBestTimes(time)"
+			@timerReset="() => resetTimer = false" />
+		<div v-if="!gameOver" class="dealer-only-container">
+			<div class="dealer-only-card-hand">
+				<PlayingCard v-for="card in hand" :card="card" :key="card.id" />
+			</div>
+			<button v-if="() => hand.value.length > 1" v-for="answer in answers" :key="`guess-button-${answer.value}`"
+				@click="checkAnswer(answer.value)" class="btn-guess">{{ answer.display }}</button>
 		</div>
-		<button v-if="() => hand.value.length > 1" v-for="answer in answers" :key="answer" @click="checkAnswer(answer)"
-			class="btn-guess">{{ answer }}</button>
-	</div>
-	<div v-else class="game-over-container">
-		<h1>Game Over</h1>
-		<div class="dealer-only-button-container">
-			<button @click="goToMain">Main Menu</button>
-			<button @click="reset">Play Again</button>
+		<div v-else class="game-over-container">
+			<h1>Game Over</h1>
+			<div class="dealer-only-button-container">
+				<button @click="goToMain">Main Menu</button>
+				<button @click="reset">Play Again</button>
+			</div>
 		</div>
+		Number of wrong answers: {{ wrongAnswers }}
+		<button v-if="!gameOver" @click="goToMain">Go to main page</button>
 	</div>
-	Number of wrong answers: {{ wrongAnswers }}
-	<button v-if="!gameOver" @click="goToMain">Go to main page</button>
+	<DealerOnlyInstructions v-else />
 </template>
 
 <style scoped>
-.dealer-only-button-container{
+.instructions-button {
+	position: fixed;
+	top: 1em;
+	right: 1em;
+	font-size: 1.5rem;
+	border: 3px solid white;
+	border-radius: 50%;
+	width: 2rem;
+	height: 2rem;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	cursor: pointer;
+	font-weight: 600;
+	user-select: none;
+}
+
+.dealer-only-button-container {
 	display: flex;
 	justify-content: space-around;
 	margin: 2em;
 	flex-wrap: wrap;
 }
-.dealer-only-button-container>button{
+
+.dealer-only-button-container>button {
 	margin: 2em;
 }
 
@@ -157,7 +194,7 @@ const reset = () => {
 	height: 120px;
 	font-size: 2em;
 	text-align: center;
-	padding: 1em;
+	padding: 0.5em;
 	margin: 1em;
 }
 
